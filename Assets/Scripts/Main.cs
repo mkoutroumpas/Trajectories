d44a9faf0c09e40fd06bd7d19897148a7d843688
Marios Koutroumpas <mkoutroumpas@gmail.com>
2019-05-31 22:34:25 +0200

    Add support for continuous launches.

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,20 +1,28 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Main : MonoBehaviour {
 
-    private int _launchDelay = 5;
-    private int _restartDelay = 15;
+    private int _initialLaunchDelay = 3;
+    private int _restartSceneDelay = 15;
+    private int _nextLaunchDelay = 3;
 
     void Start () {
-        // Simple launch
-
         GameObject sphere = GameObject.Find("Sphere");
 
         LaunchableObject launchableObject = sphere.GetComponent<LaunchableObject>();
 
-        StartCoroutine(SimpleLaunch(launchableObject));
+        ////StartCoroutine(SingleLaunch(launchableObject));
+
+        StartCoroutine(MultipleLaunch(launchableObject, new List<Vector3>()
+        {
+            new Vector3(-30, 50, 30),
+            new Vector3(40, 30, -30)
+        }));
+
+        ////StartCoroutine(SingleLaunch(launchableObject, new Vector3(-10, 50, -30)));
 
         //TODO: possibly add more, complex launches here
 
@@ -25,16 +33,29 @@ public class Main : MonoBehaviour {
 		
 	}
 
-    private IEnumerator SimpleLaunch(LaunchableObject launchableObject, Vector3 force = default(Vector3))
+    private IEnumerator SingleLaunch(LaunchableObject launchableObject, Vector3 force = default(Vector3))
     {
-        yield return new WaitForSeconds(_launchDelay);
+        yield return new WaitForSeconds(_initialLaunchDelay);
 
         launchableObject.OnLaunch(force);
 
-        yield return new WaitForSeconds(_restartDelay);
+        yield return new WaitForSeconds(_nextLaunchDelay);
 
         ////SceneManager.LoadScene(0);
-        
+    }
 
+    private IEnumerator MultipleLaunch(LaunchableObject launchableObject, List<Vector3> forces = null)
+    {
+        yield return new WaitForSeconds(_initialLaunchDelay);
+
+        foreach (var force in forces)
+        {
+            if (force != null)
+            {
+                launchableObject.OnLaunch(force);
+
+                yield return new WaitForSeconds(_nextLaunchDelay);
+            }
+        }
     }
 }
