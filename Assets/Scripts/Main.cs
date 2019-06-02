361d2d3911e8ab5d91a4c0aeb7500a8f1584dbd2
Marios Koutroumpas <mkoutroumpas@gmail.com>
2019-06-02 11:48:30 +0200

    Add more sample trajectories to evaluate performance.

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -20,6 +20,10 @@ public class Main : MonoBehaviour {
         {
             new Vector3(-30, 50, 30),
             new Vector3(40, 30, -30),
+            new Vector3(10, 10, 10),
+            new Vector3(-5, 30, -5),
+            new Vector3(10, 20, 20),
+            new Vector3(-20, 20, 20),
             ////new Vector3(-10, 20, 10) // Add a third or fourth launch, etc.
         }));
 
